d951aeee58fb7d199a0cac21bb7acfee3b155b16
Lou7925 <pipichiquitito@gmail.com>
2021-03-03 21:27:54 -0300

    pequeño cambio

--- Proyecto_Final/Proyecto_Final/settings.py
+++ Proyecto_Final/Proyecto_Final/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Proyecto_Final'
+    'Proyecto_Final',
 ]
 
 MIDDLEWARE = [
