e7d71ff9c9f1857b395a0232c9a37896e6d3f89b
Lou7925 <pipichiquitito@gmail.com>
2021-03-03 21:27:00 -0300

    Creacion de tablas

--- Proyecto_Final/proyecto_web_servicios/models.py
+++ Proyecto_Final/proyecto_web_servicios/models.py
@@ -5,3 +5,21 @@ from django.db import models
 class Servicio(models.Model): 
     nombre = models.CharField(max_length=100),
     imagen = models.ImageField(upload_to="varios", null=False)
+
+class Registro_cliente(models.Model):
+    nombre = models.CharField(max_length=40),
+    apellido = models.CharField(max_length=40),
+    telefono = models.IntegerField(max_length=10),
+    direccion = models.CharField(max_length=50),
+    email = models.EmailField()
+
+class Registro_servicios(models.Model):
+    nombre = models.CharField(max_length=40),
+    apellido = models.CharField(max_length=40),
+    telefono = models.IntegerField(max_length=10),
+    direccion = models.CharField(max_length=50),
+    email = models.EmailField(),
+    matricula = models.BooleanField(),
+    servicio = models.CharField(max_length=200)
+
+    
