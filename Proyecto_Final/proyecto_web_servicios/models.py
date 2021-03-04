c41fc5488a56d68a8c439858a4ad988cf2a63bf7
Lou7925 <pipichiquitito@gmail.com>
2021-03-03 23:21:54 -0300

    Mas modelos

--- Proyecto_Final/proyecto_web_servicios/models.py
+++ Proyecto_Final/proyecto_web_servicios/models.py
@@ -3,23 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Servicio(models.Model): 
-    nombre = models.CharField(max_length=100),
+    nombre = models.CharField(max_length=100,default='charfield')
     imagen = models.ImageField(upload_to="varios", null=False)
 
 class Registro_cliente(models.Model):
-    nombre = models.CharField(max_length=40),
-    apellido = models.CharField(max_length=40),
-    telefono = models.IntegerField(max_length=10),
-    direccion = models.CharField(max_length=50),
+    nombre = models.CharField(max_length=40,default='charfield')
+    apellido = models.CharField(max_length=40, default='charfield')
+    telefono = models.IntegerField(default='integerfield')
+    direccion = models.CharField(max_length=50,default='charfield')
     email = models.EmailField()
 
 class Registro_servicios(models.Model):
-    nombre = models.CharField(max_length=40),
-    apellido = models.CharField(max_length=40),
-    telefono = models.IntegerField(max_length=10),
-    direccion = models.CharField(max_length=50),
-    email = models.EmailField(),
-    matricula = models.BooleanField(),
+    nombre = models.CharField(max_length=40,default='charfield')
+    apellido = models.CharField(max_length=40,default='charfield')
+    telefono = models.IntegerField(default='integerfield')
+    direccion = models.CharField(max_length=50,default='charfield')
+    email = models.EmailField(default='charfield')
+    matricula = models.BooleanField(default='charfield')
     servicio = models.CharField(max_length=200)
 
     
