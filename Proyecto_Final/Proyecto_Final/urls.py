603e84b0367e024c0f3c5bc2eec2cd3ab6cd55ad
Lou7925 <pipichiquitito@gmail.com>
2021-03-08 12:07:35 -0300

    Varios cambios

--- Proyecto_Final/Proyecto_Final/urls.py
+++ Proyecto_Final/Proyecto_Final/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from proyecto_web_servicios import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
